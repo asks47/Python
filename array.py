bdb03dde7e018ca720d87926baabf6b61e40e3de
asks47 <asks.aks@gmail.com>
2019-01-05 23:51:33 +0800

    Update array.py

--- array.py
+++ array.py
@@ -1,3 +1,6 @@
-import array as arr
-a = arr.array('d', [1.1, 3.5, 4.5])
-print(a)
+
+# this program will initialize array, assign value to array, find length and print array values
+import array as arr 
+a = arr.array('d', [1.1, 3.5, 4.5]) 
+for x in range(0,len(a)):
+  print(a[x])
