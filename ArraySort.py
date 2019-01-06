fbaed1ba48ec7bc4b92c5f984111c78a0ccfcd0b
asks47 <asks.aks@gmail.com>
2019-01-07 01:00:52 +0800

    Update ArraySort.py

--- ArraySort.py
+++ ArraySort.py
@@ -1,6 +1,6 @@
 import array as arr
 a = arr.array('d', [1.5, 1.4, 2.5, 1.1, 1.0]) 
-for x in range(0,len(a)):
+for x in range(0,len(a)-1):
     for y in range(x+1,len(a)):
        if a[x] > a[y]:
           #print(a[x])
